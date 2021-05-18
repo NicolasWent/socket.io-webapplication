85920f0f604cd6ea0f61c9962019cf9d2d6da756
Nicolas Went <nicolas.went@epita.fr>
2021-05-18 21:59:36 +0200

    [Username] Added more or less functionning username

--- index.js
+++ index.js
@@ -9,14 +9,24 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+users = [];
+
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log("user disconnected");
     });
 
+    const currId = users.length;
+    console.log(users);
+    console.log(currId);
+
+    socket.on('username', (username) => {
+        users.push(username);
+    });
+
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+        io.emit('chat message', users[currId] + msg);
         // socket.broadcast.emit('hi'); <= will send message to everyone except himself
     });
 });
